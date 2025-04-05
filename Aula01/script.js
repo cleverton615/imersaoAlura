672920877f2c6040c31d934ddc104888d9a416b3
CLEVERTON ALLAN DE MELO <CLEVERTON_ALLAN@OUTLOOK.COM>
2025-04-05 16:11:59 -0300

    Imersão Alura - Melhorando e incrementando nos códigos

--- Aula01/script.js
+++ Aula01/script.js
@@ -1,5 +1,32 @@
-function conversor() {
-    valorWons = prompt("Digite um valor em wons")
-    totalWons = 0.004 * valorWons
-    alert(`O valor em real do wons digitado é ${totalWons.toLocaleString('pt-BR',{style: 'currency', currency:'BRL'})}.`)
+function conversorWons() {
+  valorWons = prompt("Digite um valor em wons");
+  totalWons = 0.004 * valorWons;
+  alert(
+    `O valor em real do wons digitado é ${totalWons.toLocaleString("pt-BR", {
+      style: "currency",
+      currency: "BRL",
+    })}.`
+  );
+}
+
+function conversorDolar() {
+  valorDolar = prompt("Digite um valor em dolar");
+  totalDolar = 5.84 * valorDolar;
+  alert(
+    `O valor em real do dolar digitado é ${totalDolar.toLocaleString("pt-BR", {
+      style: "currency",
+      currency: "BRL",
+    })}.`
+  );
+}
+
+function conversorEuro() {
+  valorEuro = prompt("Digite um valor em euro");
+  totalEuro = 6.4 * valorEuro;
+  alert(
+    `O valor em real do euro digitado é ${totalEuro.toLocaleString("pt-BR", {
+      style: "currency",
+      currency: "BRL",
+    })}`
+  );
 }
--- Aula03/script.js
+++ Aula03/script.js
@@ -1,22 +1,41 @@
 function jogar() {
-    ganhou = 0 //Se for 1, perdeu. Se for 0, ganhou.
-    for (rodada = 1; rodada <= 3; rodada++) {
+  let ganhou = true; // Inicialmente assume que o jogador ganhou
+  for (let rodada = 1; rodada <= 3; rodada++) {
     console.log("Rodada: " + rodada);
-    escolhaJogador = prompt("Nível " + rodada + ", vidro (1, 2 ou 3)?");
-    pisoQuebrado = Math.floor(Math.random() * 3) + 1;
+    let escolhaJogador;
+    let validacao = false;
 
-    if (escolhaJogador == pisoQuebrado) {
+    while (!validacao) {
+      escolhaJogador = prompt("Nível " + rodada + ", vidro (1, 2 ou 3)?");
+
+      // Verifica se a entrada é um número e está entre 1 e 3
+      if (
+        !isNaN(escolhaJogador) && parseInt(escolhaJogador) >= 1 && parseInt(escolhaJogador) <= 3
+      ) {
+        validacao = true;
+        escolhaJogador = parseInt(escolhaJogador); // Converte para número inteiro
+      } else {
+        alert("Opção inválida. Por favor, escolha entre 1, 2 ou 3.");
+      }
+    }
+
+    const pisoQuebrado = Math.floor(Math.random() * 3) + 1;
+
+    if (escolhaJogador === pisoQuebrado) {
       // Escolheu o vidro que estava quebrado
       alert("Vidro quebrou! Acabou o jogo pra você.");
-      rodada = 5;
-      ganhou = 1
+      rodada = 5; // Força a saída do loop
+      ganhou = false;
     } else {
       alert("Passou! Piso quebrado estava na ponte: " + pisoQuebrado);
     }
   }
 
-  if (ganhou == 0) {
+  if (ganhou) {
+    // Se 'ganhou' for true
     alert("Você venceu! Parabéns!");
+  } else {
+    // Se 'ganhou' for false
+    alert("Você perdeu!");
   }
-
 }
