fb5c76e201822866336bc741da3c854ee7c7a07e
CLEVERTON ALLAN DE MELO <CLEVERTON_ALLAN@OUTLOOK.COM>
2025-04-05 16:38:38 -0300

    Imersão Alura - Melhorando os códigos

--- Aula02/script.js
+++ Aula02/script.js
@@ -1,11 +1,31 @@
 function jogar() {
-  idade = prompt("Quantos anos você tem?");
-  if (idade < 18) {
-    alert("Você NÃO pode jogar jokenpo");
+  let validacaoIdade = false;
+  if (!validacaoIdade) {
+    idade = prompt("Quantos anos você tem?");
+    if (!isNaN(idade) && parseInt(idade) >= 18) {
+      validacaoIdade = true;
+      idade = parseInt(idade);
+    } else {
+      alert("Você não tem idade para jogar este jogo!");
+    }
   }
 
+  let validacao = false;
   if (idade >= 18) {
-    escolhaJogador = prompt("Digite: 1-Pedra, 2-papel ou 3-tesoura?");
+    while (!validacao) {
+      escolhaJogador = prompt("Digite: 1-Pedra, 2-papel ou 3-tesoura?");
+      // Verifica se a entrada é um número e está entre 1 e 3
+      if (
+        !isNaN(escolhaJogador) &&
+        parseInt(escolhaJogador) >= 1 &&
+        parseInt(escolhaJogador) <= 3
+      ) {
+        validacao = true;
+        escolhaJogador = parseInt(escolhaJogador); // Converte para número inteiro
+      } else {
+        alert("Opção inválida. Por favor, escolha entre 1, 2 ou 3.");
+      }
+    }
     escolhaComputador = Math.floor(Math.random() * 3) + 1; //Comando para gerar um número aleátorio entre 1 e 3
     alert("Escolha do computador: " + escolhaComputador);
     if (escolhaJogador == escolhaComputador) {
